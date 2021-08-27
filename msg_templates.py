6d7e7a30ad7e70452cae8d81694922c88e123ba8
Pavel Dubov <PADking888@yandex.ru>
2021-08-27 10:10:25 +0300

    Create weekmenu table with all day of week headers and 3 dish per day

--- msg_templates.py
+++ msg_templates.py
@@ -1,10 +1,33 @@
 from tabulate import tabulate
 
 
+def get_weekmenu(dishes_names, days_of_week, tablefmt):
+
+    # List[str] -> List[List[str, str, str]]
+    dish_name_template = '{dish_name}_{idx}'
+    dishes_per_day, dishes_per_week = [], []
+    for idx, _ in enumerate(days_of_week, 1):
+        for dish_name in dishes_names:
+            dish_fullname = dish_name_template.format(
+                dish_name=dish_name,
+                idx=idx
+            )
+            dishes_per_day.append(dish_fullname)
+        dishes_per_week.append(dishes_per_day)
+        dishes_per_day = []
+
+    # -> Dict[str: List[str, str, str]]
+    blank_table = dict(zip(days_of_week, dishes_per_week))
+
+    weekmenu = tabulate(blank_table, headers='keys', tablefmt=tablefmt)
+
+    return weekmenu
+
+
 def main():
 
     # headers for the table
-    days_of_week = (
+    days_of_week = [
         'Понедельник',
         'Вторник',
         'Среда',
@@ -12,19 +35,20 @@ def main():
         'Пятница',
         'Суббота',
         'Воскресенье'
-    )
+    ]
 
     # content for the table
-    dishes = [[
+    dishes_names = [
         'breakfast dish',
         'dish for lunch',
-        'dish for dinner'],
+        'dish for dinner'
     ]
 
     tablefmt = 'pretty'  # 'orgtbl'  # 'pretty'
-    table = tabulate(dishes, headers=days_of_week, tablefmt=tablefmt)
 
-    print(table)
+    weekmenu = get_weekmenu(dishes_names, days_of_week, tablefmt)
+
+    print(weekmenu)
 
 
 if __name__ == '__main__':
